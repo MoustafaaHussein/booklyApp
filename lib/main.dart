1b61a5e78322bb024d56288497f3896e5a19b48a
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-03-20 12:10:44 +0200

    create splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const BooklyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: true,
+    builder: (context) => BooklyApp(), // Wrap your app
+  ),
+);
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
@@ -10,7 +16,12 @@ class BooklyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     
+    return GetMaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: SplashView(),
+    );
   }
 }
