3a5f4e76313eeed2acf4b3d068815112d4d3e0b7
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-03-22 22:09:27 +0200

    Dividing screen into sections

--- lib/features/home/data/presentation/views/widgets/detalied_books_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/detalied_books_view_body.dart
@@ -1,59 +1,24 @@
-import 'package:bookly_app/features/home/data/presentation/views/widgets/book_cover.dart';
-import 'package:bookly_app/features/home/data/presentation/views/widgets/book_details.dart';
-import 'package:bookly_app/features/home/data/presentation/views/widgets/books_list_body.dart';
-import 'package:bookly_app/features/home/data/presentation/views/widgets/custom_book_button.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widgets/books_action_section.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widgets/suggested_books.dart';
 import 'package:flutter/material.dart';
 
 class DetaliedBooksViewBody extends StatelessWidget {
   const DetaliedBooksViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: EdgeInsets.only(
-                  right: width * 0.15,
-                  left: width * 0.25,
-                ),
-                child: const BookCover(),
-              ),
-
-              const Center(child: BookDetails()),
-              const Center(child: BooksRating()),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16),
-                child: Row(
-                  children: [
-                    CustomBookButton(
-                      textColor: Colors.black,
-                      displayText: r'19.99$',
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(12),
-                        topLeft: Radius.circular(12),
-                      ),
-                    ),
-                    CustomBookButton(
-                      backgroundColor: Color(0xffEF8262),
-                      displayText: r'Free preview',
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Expanded(child: SizedBox(height: 50)),
-              const SuggestedBooks(),
-              const SizedBox(height: 40),
+              BookDetailsSection(),
+              BooksActionSection(),
+              Expanded(child: SizedBox(height: 50)),
+              SuggestedBooks(),
+              SizedBox(height: 40),
             ],
           ),
         ),
