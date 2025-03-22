8a0843612fed383a8ad3b70ca5a666ce3bb51f21
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-03-23 01:35:11 +0200

    Implements fetch newest books method

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly_app/features/home/data/models/books_model/books_model.dar
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BooksModel>>> fetchNewestBooks();
   Future<Either<Failure, List<BooksModel>>> fetchFeaturedBooks();
 }
