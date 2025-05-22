85275b637146ef91423882367453ff693cfbf5b6
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-05-22 18:45:18 +0300

    Implement FetchSimilarBooksUseCase to retrieve similar books based on category

---
+++ lib/features/home/domain/use_cases/fetch_similar_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/domain/entites/book_entity/book_entity.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:bookly_app/features/home/domain/use_cases/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, String> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase(this.homeRepo);
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([String? category]) async {
+    return await homeRepo.fetchSimilarBooks(category: category!);
+  }
+}
