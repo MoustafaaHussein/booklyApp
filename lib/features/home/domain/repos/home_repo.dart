c44e235472c8b88142907505700eb3fa88d6e302
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-05-22 17:37:20 +0300

    refactor: Update HomeRepo to use BookEntity instead of BooksModel

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,15 +1,15 @@
 import 'package:bookly_app/core/errors/failure.dart';
-import 'package:bookly_app/core/models/books_model/books_model.dart';
+import 'package:bookly_app/features/home/domain/entites/book_entity/book_entity.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BooksModel>>> fetchNewestBooks({
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks({
     required String category,
   });
-  Future<Either<Failure, List<BooksModel>>> fetchFeaturedBooks({
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks({
     required String category,
   });
-  Future<Either<Failure, List<BooksModel>>> fetchSimilarBooks({
+  Future<Either<Failure, List<BookEntity>>> fetchSimilarBooks({
     required String category,
   });
 }
